ebc3e145e722e96da3c5b605215c1aebc6ef6d60
unknown <Awe Elizabeth>
2023-09-11 12:29:39 +0100

    Updated URL

--- server.js
+++ server.js
@@ -13,8 +13,13 @@ const server = app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
 
+app.get('/',  (req, res) => {
+    res.status(200).json({
+        message: "Hello World"
+    })
+});
 
-app.get('/', (req, res, next) => {
+app.get('/api', (req, res, next) => {
 
     const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
     const date = new Date();
